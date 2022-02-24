0490a6de09f7a115e4431fd9df9058f553f2b405
gyounghee <ghl0209@naver.com>
2022-02-24 17:01:24 +0900

    시저암호 - 다른사람풀이 추가

--- Algorithm_python/2022/programmers/시저암호.py
+++ Algorithm_python/2022/programmers/시저암호.py
@@ -31,3 +31,16 @@ solution(s,n)
 # TEST CASE Ⅰ   →  "ZzYy"
 s, n = "AaZz" , 25
 solution(s,n)
+
+
+
+# 다른 사람 풀이
+def caesar(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+
+    return "".join(s)
